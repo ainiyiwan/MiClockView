09be29895c7340b59554392fc6b3da8ec614f4df
Jack <ainiyiwanxxl@gmail.com>
2017-09-26 10:39:15 +0800

    Initial commit

---
+++ app/src/org/avatarqing/miclock/Main2Activity.java
@@ -0,0 +1,15 @@
+package org.avatarqing.miclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import org.lq.miclock.R;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sample_zyclock_view);
+    }
+}
